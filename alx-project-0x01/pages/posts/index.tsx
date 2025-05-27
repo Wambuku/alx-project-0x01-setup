791f605fd32c2683819d4c608decf799a1deb322
Juliet Wambuku <juliet@buupass.com>
2025-05-27 13:55:33 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,8 @@
+import Header from "@/components/layout/Header";
 const PostsPage: React.FC = () => {
     return (
       <div className="p-6">
+        <Header />
         <h1 className="text-2xl font-bold">Posts Page</h1>
       </div>
     );
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
     return (
       <div className="p-6">
+        <Header />
         <h1 className="text-2xl font-bold">Users Page</h1>
       </div>
     );
